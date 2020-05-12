90c5bebb92baee5251955524da19fd09dce95191
Tarun Kumar Sukhu <tksukhu@gmail.com>
2020-05-12 11:47:55 +0530

    added stencil component

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,13 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'intermix-design-system',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: 'intermix-design-system',
+      proxiesFile: '../intermix-library-react/src/components.ts',
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader'
